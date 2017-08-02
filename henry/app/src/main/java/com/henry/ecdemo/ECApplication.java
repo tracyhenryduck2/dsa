0c716f8b657c1ba7ffb62cb943dbebed944c47a3
tracyhenry <duckstime@sina.com>
2017-08-02 12:13:01 +0800

    d

--- henry/app/src/main/java/com/henry/ecdemo/ECApplication.java
+++ henry/app/src/main/java/com/henry/ecdemo/ECApplication.java
@@ -1,15 +1,4 @@
-/*
- *  Copyright (c) 2015 The CCP project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a Beijing Speedtong Information Technology Co.,Ltd license
- *  that can be found in the LICENSE file in the root of the web site.
- *
- *   http://www.yuntongxun.com
- *
- *  An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- */package com.henry.ecdemo;
+package com.henry.ecdemo;
 
 import android.app.Activity;
 import android.app.ActivityManager;
